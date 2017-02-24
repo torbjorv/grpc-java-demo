9777637f235b58d8386e236e2285048ad094e8d1
Torbjorn Vik <torbjorv@gmail.com>
2017-02-23 17:28:16 -0800

    add one more function

--- src/main/java/com/slb/grpc/myapi/MyServiceImpl.java
+++ src/main/java/com/slb/grpc/myapi/MyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.slb.grpc.myapi;
 
+import com.google.common.collect.Lists;
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 
@@ -11,6 +12,17 @@ public class MyServiceImpl extends MyServiceGrpc.MyServiceImplBase {
         MyApiProto.HealthCheckResponse response = MyApiProto.HealthCheckResponse
                 .newBuilder()
                 .setStatus("ok")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void listFish(MyApiProto.FishyRequest request, StreamObserver<MyApiProto.FishyResponse> responseObserver) {
+
+        MyApiProto.FishyResponse response = MyApiProto.FishyResponse.newBuilder()
+                .addAllFishNames(Lists.newArrayList("cod", "shark", "moby dick"))
                 .build();
 
         responseObserver.onNext(response);
