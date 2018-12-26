9af13334078bdb42a9b47931a132b6f9c0a6925a
Torbjorn Vik <torbjorv@gmail.com>
2018-12-26 07:46:00 -0800

    cleanup and library upgrades

--- src/main/java/com/slb/grpc/myapi/MyClient.java
+++ src/main/java/com/slb/grpc/myapi/MyClient.java
@@ -2,27 +2,20 @@ package com.slb.grpc.myapi;
 
 import com.google.protobuf.Empty;
 import io.grpc.*;
+import io.grpc.stub.MetadataUtils;
 import org.apache.commons.cli.*;
-import org.omg.PortableInterceptor.Interceptor;
-
-import java.util.concurrent.TimeUnit;
 
 public class MyClient {
 
+    private static Metadata.Key<String> API_KEY_HEADER =
+            Metadata.Key.of("x-api-key", Metadata.ASCII_STRING_MARSHALLER);
+
     public static void main(String[] args) throws Exception {
 
-        Channel channel = getChannel(args);
-
-        MyServiceGrpc.MyServiceBlockingStub blockingStub = MyServiceGrpc.newBlockingStub(channel);
-        blockingStub.ping(Empty.getDefaultInstance());
-    }
-
-    static private Channel getChannel(String[] args) throws ParseException {
         Options options = new Options();
         options.addOption(Option.builder().longOpt("host").optionalArg(false).hasArg().desc("host").build());
         options.addOption(Option.builder().longOpt("port").optionalArg(false).hasArg().desc("port").build());
         options.addOption(Option.builder().longOpt("key").optionalArg(false).hasArg().desc("API key").build());
-
         CommandLineParser parser = new DefaultParser();
         CommandLine line = parser.parse(options, args);
 
@@ -30,52 +23,19 @@ public class MyClient {
         int port = Integer.parseInt(line.getOptionValue("port"));
         String apiKey = line.getOptionValue("key");
 
+
         Channel channel = ManagedChannelBuilder.forAddress(host, port)
                 .directExecutor()
-                .usePlaintext(true)
+                .usePlaintext()
                 .build();
+        
+        Metadata headers = new Metadata();
+        headers.put(API_KEY_HEADER, apiKey);
 
-        channel = ClientInterceptors.intercept(channel,  new ApiKeyInterceptor(apiKey, ""));
-        return channel;
-    }
+        MyServiceGrpc.MyServiceBlockingStub blockingStub = MyServiceGrpc.newBlockingStub(channel);
+        blockingStub = MetadataUtils.attachHeaders(blockingStub, headers);
 
-    private static final class ApiKeyInterceptor implements ClientInterceptor {
-        private final String apiKey;
-        private final String authToken;
-
-        private static Metadata.Key<String> API_KEY_HEADER =
-                Metadata.Key.of("x-api-key", Metadata.ASCII_STRING_MARSHALLER);
-        private static Metadata.Key<String> AUTHORIZATION_HEADER =
-                Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER);
-
-        public ApiKeyInterceptor(String apiKey, String authToken) {
-            this.apiKey = apiKey;
-            this.authToken = authToken;
-        }
-
-        @Override
-        public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
-                MethodDescriptor<ReqT, RespT> method,
-                CallOptions callOptions,
-                Channel next) {
-
-            ClientCall<ReqT, RespT> call = next.newCall(method, callOptions);
-
-            call = new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(call) {
-                @Override
-                public void start(Listener<RespT> responseListener, Metadata headers) {
-
-                    if (apiKey != null && !apiKey.isEmpty()) {
-                        headers.put(API_KEY_HEADER, apiKey);
-                    }
-
-                    if (authToken != null && !authToken.isEmpty()) {
-                        headers.put(AUTHORIZATION_HEADER, "Bearer " + authToken);
-                    }
-                    super.start(responseListener, headers);
-                }
-            };
-            return call;
-        }
+        MyApiProto.HealthCheckResponse response = blockingStub.ping(Empty.getDefaultInstance());
+        System.out.println(response.toString());
     }
 }
